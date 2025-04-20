a424d3bb3f7bbfb15b4fe55b0d729915100321cc
khh-codes <khh.code.98@gmail.com>
2025-04-20 10:22:20 +0530

    fe/feat: integrate list-api

--- backend/dist/models/User.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userModel = void 0;
-const mongoose_1 = require("mongoose");
-const User_1 = require("../schema/User");
-exports.userModel = (0, mongoose_1.model)("User", User_1.userSchema);
